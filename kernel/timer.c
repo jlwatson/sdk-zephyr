e5e6e99c356940689186b7014bd4c850c76f2263
Jean-Luc Watson <jlwatson@eecs.berkeley.edu>
2020-05-25 10:49:28 -0700

    fixes to timer updates

--- kernel/timer.c
+++ kernel/timer.c
@@ -55,7 +55,9 @@ void z_timer_expiration_handler(struct _timeout *t)
 
 #ifdef CONFIG_LIVE_UPDATE
     if(lu_trigger_on_timer(timer)) {
-        lu_update_at_timer(timer);
+        printk("timer before: %p\n", timer);
+        lu_update_at_timer(&timer);
+        printk("timer after: %p\n", timer);
     }
 #endif // CONFIG_LIVE_UPDATE
 
